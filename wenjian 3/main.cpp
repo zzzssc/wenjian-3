cfad3437bf7c24e1a26426b05d5c1e0594c1b496
zzzssc <1556837456@qq.com>
2019-06-26 21:31:04 +0800

    Initial Commit

---
+++ wenjian 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  wenjian 3
+//
+//  Created by s20181106119 on 2019/6/26.
+//  Copyright © 2019 s20181106119. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
